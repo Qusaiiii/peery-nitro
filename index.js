148907e12c9584b181a63eaa7879fb45c95ffe1d
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2019-01-28 07:09:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
 if (message.content === '!nitro') {
-     if(!message.author.id === '513730861947158556') return message.channel.send('Nice Try');
+     if(!message.author.id === '513730861947158556') return;
    message.reply('جار عمل روابط نيترو...');
    setInterval(function() {
      var links = [];
